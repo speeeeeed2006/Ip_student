43a0ea51557b6c7024059110c8d006d3f8349c9c
Caroline <carolinebleier@gmail.com>
2014-10-05 17:36:13 +0200

    html des pages index, connexion, produit et panier

---
+++ html/index.php
@@ -0,0 +1,54 @@
+<?php require_once 'header.php'; ?>
+
+			<div id="wrap">
+
+				<div id="haut">
+					<div id="slider"></div>
+					<div id="vignettes">
+						<ul>
+							<li></li>
+							<li></li>
+							<li></li>
+							<li></li>
+						</ul>
+					</div>
+				</div><!--#haut-->
+
+				<div id="bas">
+					<div id="a_la_une">
+						<h2>Catégorie 1</h2>
+						<div class="produit">
+							<img src="" alt="" />
+							<span class="marque">Marque</span>
+							<span class="nom_produit">Nom produit</span>
+							<p class="descriptif">Lorem ipsum dolor sit amet, consectetur adipisicing elistrud ullamco, consectetur adipisicing elistrud ullamco.</p>
+							<button type="button">Acheter</button>
+							<span class="prix">159 €</span>
+						</div><!-- fin .produit --><!--
+					--><div class="produit">
+							<img src="" alt="" />
+							<span class="marque">Marque</span>
+							<span class="nom_produit">Nom produit</span>
+							<p class="descriptif">Lorem ipsum dolor sit amet, consectetur adipisicing elistrud ullamco.</p>
+							<button type="button">Acheter</button>
+							<span class="prix">159 €</span>
+						</div><!-- fin .produit -->
+						<div id="plus_produits">
+							<span><a href="" title="">Voir + de produits</a></span>
+						</div> <!-- fin .#plus_produits -->
+					</div><!--#a_la_une--><!--
+				--><div id="nouveau">
+						<h2>Nouveau</h2>
+						<img src="" alt="">
+						<span class="marque">Marque</span>
+						<span class="nom_produit">Nom produit</span>
+						<p class="descriptif">Lorem ipsum dolor sit amet, consectetur adipisicing elistrud ullamco.</p>
+						<button type="button">Acheter</button>	
+						<span class="prix">159 €</span>
+					</div><!-- fin #nouveau -->	
+				</div> <!-- fin #bas -->
+
+			</div><!--#wrap -->
+		 <?php require_once 'widgets.php'; ?>
+		
+<?php require_once 'footer.php'; ?>
